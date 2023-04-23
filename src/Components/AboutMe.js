a86fb6ed20ca3b3da8e65b3fc986eda46e1d1b3d
lilkahuna <90534314+lilkahuna@users.noreply.github.com>
2023-04-23 01:05:32 -0400

    Added more to the about me section

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -3,11 +3,12 @@ import './AboutMe.css'
 const AboutMe = () =>{
     return(
         <>
+       
+
             <div className="AB-div">
                 
                 <p className='text'>
                 
-                My name is Brodie and I am an aspiring software engineer and cyber security analyst. 
                 I have been coding for about a year now and I can program in multiple languages. Some of these languages include Python, Javascript, Java, and C#. 
                 I first learned how to code through a python course I took as a child but ended up quitting. 
                 After this I decided I wanted to learn programming again so I applied to a STEM academy near me. 
@@ -15,13 +16,38 @@ const AboutMe = () =>{
                 After I completed this class I was able to take an intro to programming class. 
                 The class stated us out with Java but then moved into unity where we used C#. 
                 At the end of the class I ended up liking programming so much I continued to program even after the class was over and
-                this is how I learned python and javascript. 
-                After I learned basic javascript, HTML, and CSS I started to learn React which is what this website is made with.
+                this is how I learned Python and Javascript. 
+                After I learned basic javascript, HTML, and CSS I started to learn React which is what this web app is made with and I will continue to learn new things about
+                programming and software development.
 
                 </p>
 
             </div>
         
+
+            {/*For testing reasons */}
+            <div className='CS-div'>
+                
+                <p className='text'>
+                
+                I have been coding for about a year now and I can program in multiple languages. Some of these languages include Python, Javascript, Java, and C#. 
+                I first learned how to code through a python course I took as a child but ended up quitting. 
+                After this I decided I wanted to learn programming again so I applied to a STEM academy near me. 
+                I spent my freshman year at the STEM academy taking a fundamentals class. 
+                After I completed this class I was able to take an intro to programming class. 
+                The class stated us out with Java but then moved into unity where we used C#. 
+                At the end of the class I ended up liking programming so much I continued to program even after the class was over and
+                this is how I learned Python and Javascript. 
+                After I learned basic javascript, HTML, and CSS I started to learn React which is what this web app is made with and I will continue to learn new things about
+                programming and software development.
+               
+                </p>
+
+            </div>
+            
+
+        
+
         </>
 
 
