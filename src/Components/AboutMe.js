5b036886adf3f414bc919f4ae175b8c8c3698683
lilkahuna <90534314+lilkahuna@users.noreply.github.com>
2023-04-22 20:27:45 -0400

    TEST COMMIT

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -14,8 +14,8 @@ const AboutMe = () =>{
                 I spent my freshman year at the STEM academy taking a fundamentals class. 
                 After I completed this class I was able to take an intro to programming class. 
                 The class stated us out with Java but then moved into unity where we used C#. 
-                At the end of the class I ended up liking programming so much I continued to program even after the class was over. 
-                This is how I learned python and javascript. 
+                At the end of the class I ended up liking programming so much I continued to program even after the class was over and
+                this is how I learned python and javascript. 
                 After I learned basic javascript, HTML, and CSS I started to learn React which is what this website is made with.
 
                 </p>
