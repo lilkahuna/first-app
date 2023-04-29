716ca660e333b2eefb6105e1954bdaa70bc4e968
lilkahuna <90534314+lilkahuna@users.noreply.github.com>
2023-04-29 00:36:47 -0400

    Worked on a projects section

--- src/Components/Github.js
+++ src/Components/Github.js
@@ -1,42 +1,24 @@
-import { useEffect, useState } from 'react'
 import './Github.css'
-
-const { REACT_APP_API_KEY } = process.env
-
-
-
 
 const Git = () => {
 
-    //set use state to array
-    const [info, setInfo] = useState(0)
-
-    //use set data to allow us to use the info variable to display information retrived from the api
-    useEffect(() => {
-        fetch("https://github-contributions-api.jogruber.de/v4/lilkahuna")
-            .then((result) => result.json())
-            .then((data) => {
-                console.log(data)
-                setInfo(data)
-            })
-
-    }, [])
-
-
     //? is a safty operator
     return (
-        
+
         <>
+
             <div className='git-div'>
-                <h1 className='main-head'>How Active Am I?</h1>
+                <h1 className='main-head'>Projects I Have Done</h1>
+                
+                <div className='project-1'>
+                    <h2 className='pro-head'>Basic Weather App</h2>
+                </div>
 
-                <h2 className='sec-head'>Total Contributions</h2>
-                <p className='text'>{info?.total}</p>
-
+                <div className='project-2'>
+                    <h2 className='pro-head'>Excel Script</h2>
+                </div>
             </div>
-            
 
-            
         </>
 
     )
