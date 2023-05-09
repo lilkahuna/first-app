e4202dcd28b4aef66fc784f4d1a6f7c523aeb3e1
lilkahuna <brodie.liverman@gmail.com>
2023-05-08 20:48:43 -0400

    Finishing up the card design

--- src/Components/Github.js
+++ src/Components/Github.js
@@ -10,19 +10,25 @@ const Git = () => {
             <div className='git-div'>
                 <h1 className='main-head'>Projects I Have Done</h1>
                 
-                <div className='project-1'>
+                <a className='linked' href='https://github.com/lilkahuna/Python-Projects/tree/main/Python%20Scripts'><div className='project-1'>
                         <img src=''></img>
                         <h1>Basic Weather App</h1>
+                        
                         <div className='project1-body'>
-                            <p>This is a basic weather app I created in Python. It utilizes a </p>
+                            <p>This is a basic weather app I created in Python. It utilizes a library called Custom Tkinter.</p>
                         </div>
-                </div>
+                </div></a>
 
-                <div className='project-2'>
+                <a className='linked' href='https://github.com/lilkahuna/Python-Projects/tree/main/Python%20Scripts'><div className='project-2'>
                     <h1 className='pro-head'>Excel Script</h1>
-                </div>
+                    
+                    
+                    <div className='project2-body'>
+                            <p>This is a basic excel script I created in Python. It uses a library called OpenPyXl which is used to add 
+                                functionality to the script. It was made for my dad so he could copy and paste to certain colums without ruining formating.</p>
+                    </div>
+                </div></a>
             </div>
-
         </>
 
     )
