707f67bf15db2169145c2f9a031043add376589d
lilkahuna <brodie.liverman@gmail.com>
2023-05-29 13:51:26 -0400

    Email counter finished, will make the site responsive soon.

--- src/Components/Email.js
+++ src/Components/Email.js
@@ -2,41 +2,43 @@ import React, { useRef, useState, useEffect } from 'react';
 import emailjs from '@emailjs/browser';
 import './Email.css'
 import { setDoc, doc } from "firebase/firestore";
-import { db, fireBaseConfig } from "./firebase.js"
-import { collection, getDocs, updateDoc, increment } from  'firebase/firestore'
+import { db } from "./firebase.js"
+import { collection, getDocs, getDoc, updateDoc, increment } from 'firebase/firestore'
+import { AnimationOnScroll } from 'react-animation-on-scroll';
+import { animate__fadeInDown } from 'animate.css'
 
 const Email = () => {
     //using refs to get information
     const form = useRef()
     const name = useRef()
-    //makes and array of objects
+    const email = useRef()
+    const message = useRef()
+    //makes an array of objects
     const [names, setNames] = useState([])
     const [counter, setCounter] = useState(0)
 
-    
-    useEffect(()=> {
+
+    useEffect(() => {
         getData()
     }, [])
 
     //runs on change of the state variable "names"
     useEffect(() => {
         console.log(names)
-        console.log(counter)
     }, [names])
 
-    const getData = () =>{
-        const counterCollectionRef = collection(db, 'email_counter')
-        getDocs(counterCollectionRef).then(response => {
-            const rawCounter = response.docs.map(doc=>({
-                counter_number: doc.data(),
-                id: doc.id
-            }))
-            setCounter(rawCounter)
-        }).catch(error=> console.log("error"))
+    //get doc gets one document while get docs retrives multiple
+    const getData = () => {
+        const counterCollectionRef = doc(db, 'email_counter', 'email_counter')
+        getDoc(counterCollectionRef).then(response => {
+
+            setCounter(response.data())
+
+        }).catch(error => console.log("error"))
 
         const nameCollectionRef = collection(db, 'users')
-        getDocs(nameCollectionRef).then(response =>{
-            const rawNames = response.docs.map(doc =>({
+        getDocs(nameCollectionRef).then(response => {
+            const rawNames = response.docs.map(doc => ({
                 //retrives names
                 data: doc.data(),
                 //retrives doc id
@@ -45,26 +47,37 @@ const Email = () => {
             setNames(rawNames)
         }).catch(error => console.log('error'))
     }
-    
+
     const sendData = () => {
         const sendDataRef = doc(collection(db, 'users'))
         const incrementDataRef = doc(db, 'email_counter', 'email_counter')
         setDoc(sendDataRef, {
-            name: name.current.value
+            name: name.current.value,
+            email: email.current.value
         }).then(response => {
-            console.log(response)
 
         }).catch(error => {
-            console.warn('error')})
+            console.warn('error')
+        })
 
         updateDoc(incrementDataRef, {
             counter: increment(1)
         })
     }
 
-    const sendEmail = async(e) => {
+    const sendEmail = async (e) => {
         e.preventDefault()
-        sendData()
+
+        //shortened refrences to the value of each ref
+        const nameRef = name.current.value
+        const emailRef = email.current.value
+        const messageRef = message.current.value
+
+        if (nameRef.length <= 3 && emailRef.length <= 3 && messageRef.length <= 3) {
+            window.confirm('Please type more')
+        } else {
+            sendData()
+        }
 
         /**emailjs.sendForm('service_e7d5rye', 'template_usqbzod', form.current, 'YlBlgsE3qya_lU-_j')
             .then((result) => {
@@ -73,40 +86,44 @@ const Email = () => {
             }, (error) => {
                 console.log(error.text);
             });**/
-        
+
         //deletes everthing in the form
         //e.target.reset()
     }
 
     return (
         <>
+            <div className='form-container'>
 
-            <div className="container-1">
+                <AnimationOnScroll animateIn='animate__fadeInDown' delay={1000}>
+                    <div className="container-1">
 
-                <form className="form" ref={form} onSubmit={sendEmail}>
+                        <form className="form" ref={form} onSubmit={sendEmail}>
 
-                    <div className="descr">Contact me</div>
-                    <div className="input">
-                        <input ref={name} name='user_name' required="" autocomplete="off" type="text"/>
-                            <label for="user_name">Name</label>
+                            <div className="descr">Contact me</div>
+                            <div className="input">
+                                <input ref={name} name='user_name' required="" autocomplete="off" type="text" />
+                                <label for="user_name">Name</label>
+                            </div>
+
+                            <div className="input">
+                                <input ref={email} required="" autocomplete="off" name="user_email" type="text" />
+                                <label for="user_email">E-mail</label>
+                            </div>
+
+                            <div className="input">
+                                <textarea ref={message} name='message' required="" cols="30" rows="1" id="message"></textarea>
+                                <label for="message">Message</label>
+                            </div>
+
+                            <button type='submit' name='submit_button'>Send message →</button>
+                            <h4 className='data'>Emails sent: {counter.counter}</h4>
+                        </form>
+
                     </div>
+                </AnimationOnScroll>
 
-                    <div className="input">
-                        <input required="" autocomplete="off" name="user_email" type="text"/>
-                            <label for="user_email">E-mail</label>
-                    </div>
-
-                    <div className="input">
-                        <textarea name='message' required="" cols="30" rows="1" id="message"></textarea>
-                        <label for="message">Message</label>
-                    </div>
-
-                    <button type='submit' name='submit_button'>Send message →</button>
-                    
-                </form>
-                
             </div>
-
         </>
     );
 };
